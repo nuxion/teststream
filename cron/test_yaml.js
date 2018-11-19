5f9dad9b685b35ec35ac088b6aa2a297e25a3b90
nuxion <nuxion@gmail.com>
2018-11-18 21:12:52 -0300

    cleaning test_yaml

--- cron/test_yaml.js
+++ cron/test_yaml.js
@@ -2,16 +2,53 @@ const yaml = require('js-yaml');
 const fs   = require('fs');
 const Redis = require('ioredis');
 
-var redis = new Redis({ host: 'redis-stream' });
+var redisc = new Redis({ host: 'redis-stream' });
+const yamlfile = './w1r1.yml';
 
+var loadConfig = async (file, configHandler, taskHandler) => {
 
+    var doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
 
+    config = configHandler(doc);
 
-try {
-    var doc = yaml.safeLoad(fs.readFileSync('./w1r1.yml', 'utf8'));
-    console.log(doc);
-} catch (e){
-    console.log(e);
+    tasks = doc.tasks.map(async (item) => { await taskHandler(item.task) })
+    await Promise.all([config, tasks]);
+
 }
 
+var addConfig = (redis) => async (config) => {
+
+    try{
+       await redis.multi()
+            .hmset('stream', 'h', config.stream_server.host, 'p', config.stream_server.port)
+            .set('ver', config.version)
+            .set('shard', config.shard)
+            .exec();
+    } catch(e){
+        console.log(e);
+    }
+}
+
+var addTask = (redis) => async (task) => {
+    console.log(task.id + "w" + task.when + "o" + task.owner);
+    console.log(task)
+    try {
+       await redis.multi()
+                .hmset('tid:'+task.id, 'w', task.when, 'o', task.owner)
+                .rpush('tids:1',task.id)
+                .exec();    
+    } catch(e){
+        console.log(e);
+    }
+}
+
+var addTaskRedis = addTask(redisc);
+var addConfigRedis = addConfig(redisc);
+
+console.log(typeof(addTaskRedis))
+loadConfig(yamlfile, addConfigRedis, addTaskRedis).then((result) => {
+    console.log("Finish...");
+    redisc.disconnect()
+    redisc.quit()
+});
 
